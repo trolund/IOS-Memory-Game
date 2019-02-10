8e9d1fa91001c3350dc1aa2db3c22c0d0382cf7f
trolund@gmail.com <vxh77kdp>
2019-02-10 14:54:18 +0100

    Initial Commit

---
+++ MemoryGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by Troels on 10/02/2019.
+//  Copyright © 2019 Troels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
